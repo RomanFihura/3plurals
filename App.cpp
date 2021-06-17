b8be219e7638a7b2e51856c1a5ec25fb17a489c5
RomanFihura <79198112+RomanFihura@users.noreply.github.com>
2021-06-17 11:37:12 +0300

    Update App.cpp

--- App.cpp
+++ App.cpp
@@ -1,33 +1,24 @@
 #include "App.h"
 
-void partition(std::vector<int> &v, int low, int high, int pivot)
+void partition(std::vector<int>& v, int low, int high, int pivot)
 {
-    int i = (low - 1);
+	int temp=0;
+	int i = low ;
 
-    for (int j = low; j <= high - 1; j++)
-    {
-        if (v.at(j) < pivot)
-        {
-            i++;
-            std::swap(v.at(i), v.at(j));
-        }
-    }
-    std::swap(v.at(i + 1), v.at(high));
-    int temp;
-    for (int i = 0;; i++)
-    {
-        if (pivot < v.at(i))
-        {
-            temp = i;
-            break;
-        }
-    }
-    for (int j = 0; j < v.size(); j++)
-    {
-        if (v.at(j) == pivot)
-        {
-            v.insert(v.begin() + temp, v.at(j));
-            v.erase(v.begin() + j + 1);
-        }
-    }
+	for (int j = low; j <= high ; j++)
+	{
+		if (v.at(j) == pivot)
+		{
+			std::swap(v.at(j), v.at(temp));
+			//j++;
+			temp++;
+		}
+		if (v.at(j) < pivot)
+		{
+			i++;
+			std::swap(v.at(i), v.at(j));
+		}
+	}
+	std::swap(v.at(i + 1), v.at(high));
+	
 }
